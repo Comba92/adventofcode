dbdef7981c733d5537b907e896f7b2ade46f8feb
comba <36135770+Comba92@users.noreply.github.com>
2025-08-03 01:41:24 +0200

    aoc 2024 day 9 pt2 fail

--- 2024/src/bin/09.rs
+++ 2024/src/bin/09.rs
@@ -2,102 +2,147 @@ use std::{collections::{BTreeMap, BTreeSet}, iter};
 
 fn main() {
   let input = include_str!("09.txt").trim();
-  let mut decoded1 = Vec::new();
 
-  let mut file_blocks = Vec::new();
-  // key: block size, val: set of blocks idx start with that size
-  let mut free_blocks = BTreeMap::new();
-  
-  for (i, c) in input.char_indices() {
-    let n = c.to_digit(10).unwrap() as usize;
+  let map = input.chars()
+    .map(|c| c.to_digit(10).unwrap() as isize);
 
-    let vals = if i % 2 == 0 {
-      file_blocks.push((decoded1.len(), n));
-      iter::repeat_n(i as i32 / 2, n)
-    } else {
-      if n != 0 {
-        free_blocks.entry(n)
-          .and_modify(|e: &mut BTreeSet<usize>| { e.insert(decoded1.len()); })
-          .or_insert(BTreeSet::from([decoded1.len()]));
-      }
-
-      iter::repeat_n(-1, n)
-    };
-
-    decoded1.extend(vals);
-  }
-
-  let mut decoded2 = decoded1.clone();
-
-  let mut right = decoded1.len()-1;
-  // skip all empty blocks to the right
-  while decoded1[right] == -1 { right -= 1; }
-
-  for left in 0..decoded1.len() {
-    if decoded1[left] == -1 {
-      decoded1.swap(left, right);
-      right -= 1;
-      while decoded1[right] == -1 { right -= 1; }
-    }
-
-    if left >= right { break; }
-  }
-
-  // println!("{decoded2:?}");
-  // println!("{}", decoded2.len());
-  // println!("Free blocks: {free_blocks:?} - Files: {file_blocks:?}");
-
-  for (file_start, file_len) in file_blocks.iter().rev().copied() {
-    let mut best_free = free_blocks.range(file_len..);
-
-    if let Some((block_len, _)) = best_free.next() {
-      let block_len = *block_len;
-
-      let blocks = free_blocks.get_mut(&block_len).unwrap();
-      let block_start = blocks.iter().find(|block_start| **block_start < file_start).copied().unwrap();
-      let file_id = decoded2[file_start];
-      
-      decoded2
-      .splice(block_start..block_start+file_len, iter::repeat_n(file_id, file_len));
-      decoded2
-      .splice(file_start..file_start+file_len, iter::repeat_n(-1, file_len));
-    
-      blocks.remove(&block_start);
-      // println!("{file_id}");
-
-      if blocks.is_empty() {
-        free_blocks.remove(&block_len);
-      }
-      
-      if file_len < block_len {
-        let new_block_start = block_start + file_len;
-        
-        free_blocks.entry(block_len - file_len)
-          .and_modify(|e: &mut BTreeSet<usize>| { e.insert(new_block_start); })
-          .or_insert(BTreeSet::from([new_block_start]));
-      }
-
-      // println!("{free_blocks:?}");
-    }
-  }
-
-  // println!();
-  // println!("{decoded2:?}");
-  // println!("{}", decoded2.len());
-  // println!("{free_blocks:?} {file_blocks:?}");
-
-  let res1 = checksum(&decoded1);
-  // 8484226147322 - too high
-  let res2 = checksum(&decoded2);
-
-  println!("{res1}");
-  println!("{res2}");
-}
-
-fn checksum(list: &[i32]) -> usize {
-  list.iter()
+  // get file blocks lengths list
+  let files = map.clone()
     .enumerate()
-    .filter(|(_, n)| **n != -1)
-    .map(|(i, n)| *n as usize  * i)
-    .sum::<usize>()
+    .filter_map(|b| (b.0 % 2 == 0).then_some(b.1))
+    .collect::<Vec<_>>();
+
+  // get empty blocks lengths list
+  let empties = map.clone()
+    .enumerate()
+    .filter_map(|b| (b.0 % 2 == 1).then_some(b.1))
+    .collect::<Vec<_>>();
+
+  let mut mem = Vec::new();
+
+  let mut file_blocks_count = 0;
+  // first pass: add file_blocks to disk
+  // iter files togheter with empty spaces
+  for (id, (file_len, empty_len)) in files.iter()
+    .zip(empties.iter())
+    .enumerate()
+  {
+    let file_blocks = iter::repeat_n(id as isize, *file_len as usize);
+    mem.extend(file_blocks);
+    file_blocks_count += *file_len;
+
+    let empty_blocks = iter::repeat_n(-1, *empty_len as usize);
+    mem.extend(empty_blocks);
+  }
+  // push last
+  let last = *files.last().unwrap();
+  let file_blocks = iter::repeat_n(files.len() as isize-1, last as usize);
+  mem.extend(file_blocks);
+  file_blocks_count += last;
+
+  
+  let mut files_clone = files.clone();
+  let mut mem1 = mem.clone();
+
+  // second pass: add empty_blocks to disk
+  for block in mem1
+    .iter_mut()
+    .filter(|b| **b == -1)
+  {
+    if files_clone.is_empty() { break; }
+
+    let id = files_clone.len() as isize - 1;
+    let last = files_clone.last_mut().unwrap();
+
+    *block = id;
+    *last -= 1;
+    if *last <= 0 { files_clone.pop(); }
+  }
+  mem1.truncate(file_blocks_count as usize);
+
+
+  let mut file_starts = Vec::new();
+  let mut empty_blocks: BTreeMap<isize, BTreeSet<usize>> = BTreeMap::new();
+
+  let mut cumsum = 0;
+  // build file_blocks<index, length> list
+  // build empty_blocks<length, Set<indexes>> map 
+  for (file_len, empty_len) in map.clone()
+    .step_by(2)
+    .zip(map.skip(1).step_by(2))
+  {
+    file_starts.push(cumsum);
+    cumsum += file_len as usize;
+
+    if empty_len > 0 {
+      empty_blocks.entry(empty_len)
+        .or_default()
+        .insert(cumsum);
+    }
+
+    cumsum += empty_len as usize;
+  }
+  // push last
+  file_starts.push(cumsum);
+
+  let mut mem2 = mem.clone();
+  for (file_id, file_len) in files
+    .iter()
+    .enumerate()
+    .rev()
+  {
+    // if there is any free space of file_len
+    let free_blocks = empty_blocks
+      .range_mut(file_len..)
+      .skip_while(|entry| entry.1.is_empty())
+      .next();
+
+    let mut should_update = None;
+    if let Some((empty_len, free_blocks)) = free_blocks {
+      // never move a file to the right
+      if *free_blocks.first().unwrap() > file_starts[file_id] { continue; }
+      
+      let empty_block_start = free_blocks.pop_first().unwrap() as usize;
+
+      // add left
+      mem2[empty_block_start .. empty_block_start+ *file_len as usize].fill(file_id as isize);
+      
+      // remove right
+      let file_block_start = file_starts[file_id];
+      mem2[file_block_start..file_block_start+*file_len as usize].fill(-1);
+      
+      println!("Moving {file_id} from {file_block_start} to {empty_block_start}");
+      should_update = Some((empty_block_start, *empty_len));
+    }
+
+    if let Some((old_empty_start, old_empty_len)) = should_update {
+      let new_empty_len = old_empty_len - *file_len;
+      let new_empty_start = old_empty_start + (old_empty_len - new_empty_len) as usize;
+      
+      if new_empty_len > 0 {
+        empty_blocks.entry(new_empty_len)
+          .or_default()
+          .insert(new_empty_start);
+      }
+    }
+
+    println!("{empty_blocks:?}");
+  }
+
+  let res1 = checksum(&mem1);
+  let res2 = checksum(&mem2);
+  println!("{res1:?} {res2:?}");
+
+  // println!("{mem2:?}");
 }
+
+fn checksum(blocks: &[isize]) -> isize {
+  blocks.iter()
+    .enumerate()
+    .filter(|(_, v)| **v != -1)
+    .map(|(i, &v)| i as isize * v)
+    .sum()
+}
+
+// 6297995201652 too low
+// 8484226147322 too high
---
+++ 2024/src/bin/09_old.rs
@@ -0,0 +1,103 @@
+use std::{collections::{BTreeMap, BTreeSet}, iter};
+
+fn main() {
+  let input = include_str!("09.txt").trim();
+  let mut decoded1 = Vec::new();
+
+  let mut file_blocks = Vec::new();
+  // key: block size, val: set of blocks idx start with that size
+  let mut free_blocks = BTreeMap::new();
+  
+  for (i, c) in input.char_indices() {
+    let n = c.to_digit(10).unwrap() as usize;
+
+    let vals = if i % 2 == 0 {
+      file_blocks.push((decoded1.len(), n));
+      iter::repeat_n(i as i32 / 2, n)
+    } else {
+      if n != 0 {
+        free_blocks.entry(n)
+          .and_modify(|e: &mut BTreeSet<usize>| { e.insert(decoded1.len()); })
+          .or_insert(BTreeSet::from([decoded1.len()]));
+      }
+
+      iter::repeat_n(-1, n)
+    };
+
+    decoded1.extend(vals);
+  }
+
+  let mut decoded2 = decoded1.clone();
+
+  let mut right = decoded1.len()-1;
+  // skip all empty blocks to the right
+  while decoded1[right] == -1 { right -= 1; }
+
+  for left in 0..decoded1.len() {
+    if decoded1[left] == -1 {
+      decoded1.swap(left, right);
+      right -= 1;
+      while decoded1[right] == -1 { right -= 1; }
+    }
+
+    if left >= right { break; }
+  }
+
+  // println!("{decoded2:?}");
+  // println!("{}", decoded2.len());
+  // println!("Free blocks: {free_blocks:?} - Files: {file_blocks:?}");
+
+  for (file_start, file_len) in file_blocks.iter().rev().copied() {
+    let mut best_free = free_blocks.range(file_len..);
+
+    if let Some((block_len, _)) = best_free.next() {
+      let block_len = *block_len;
+
+      let blocks = free_blocks.get_mut(&block_len).unwrap();
+      let block_start = blocks.iter().find(|block_start| **block_start < file_start).copied().unwrap();
+      let file_id = decoded2[file_start];
+      
+      decoded2
+      .splice(block_start..block_start+file_len, iter::repeat_n(file_id, file_len));
+      decoded2
+      .splice(file_start..file_start+file_len, iter::repeat_n(-1, file_len));
+    
+      blocks.remove(&block_start);
+      // println!("{file_id}");
+
+      if blocks.is_empty() {
+        free_blocks.remove(&block_len);
+      }
+      
+      if file_len < block_len {
+        let new_block_start = block_start + file_len;
+        
+        free_blocks.entry(block_len - file_len)
+          .and_modify(|e: &mut BTreeSet<usize>| { e.insert(new_block_start); })
+          .or_insert(BTreeSet::from([new_block_start]));
+      }
+
+      // println!("{free_blocks:?}");
+    }
+  }
+
+  // println!();
+  // println!("{decoded2:?}");
+  // println!("{}", decoded2.len());
+  // println!("{free_blocks:?} {file_blocks:?}");
+
+  let res1 = checksum(&decoded1);
+  // 8484226147322 - too high
+  let res2 = checksum(&decoded2);
+
+  println!("{res1}");
+  println!("{res2}");
+}
+
+fn checksum(list: &[i32]) -> usize {
+  list.iter()
+    .enumerate()
+    .filter(|(_, n)| **n != -1)
+    .map(|(i, n)| *n as usize  * i)
+    .sum::<usize>()
+}
